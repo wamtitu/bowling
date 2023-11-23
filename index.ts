d6b18fd371d09d0c8cef620346489efdfde04a6f
wamtitu <wamtitujose@gmail.com>
2023-11-23 12:09:54 +0300

    first commit

---
+++ index.ts
@@ -0,0 +1,50 @@
+// var FrameScores:number[] =[9,20,35];
+// var frametotal:number[] = [];
+
+
+// // function bowlingScores(Scores){
+// //     let totalscores:number = 0;
+// //   for (let i =0; i<FrameScores.length; i++){
+// //     totalscores += FrameScores[i];
+// //     console.log(totalscores);
+// //   }
+  
+// // }
+
+// // bowlingScores(FrameScores);
+
+// function FramesLogic(a:number, b:number){
+//     var total:number = 0;
+//     var sum:number = a+b;
+
+//     if(a == 10){
+//       sum = 10;
+//       FrameScores.push(sum);
+//       FrameScores.push(7);
+//       FrameScores.push(2);
+
+      
+//       FrameScores.forEach((i, index)=>{
+//         total += a[index + 1];
+//         total += a[index + 1];
+
+//         frametotal.push(total);
+
+//         console.log(FrameScores);
+//         console.log(frametotal);
+//         console.log("hello")
+//       })
+
+//     }else{
+//       console.log("not a strike")
+//     }
+// }
+
+
+// FramesLogic(10,0)
+
+ function sum(x,y){
+  return x+y;
+}
+
+export default sum;
